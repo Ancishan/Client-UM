34706b1c27eddff2ef697d9237461885bbc64b35
Ancishan <shantodev027@gmail.com>
2025-03-26 00:33:04 +0800

    [DONE] dynamic sidebar

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,38 +2,39 @@ import React from 'react';
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import { Layout, Menu, MenuProps, theme } from 'antd';
 import { NavLink, Outlet } from 'react-router-dom';
+import { adminSidebarItems } from '../../routes/admin.routes';
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items: MenuProps['items'] = [
-  {
-    key: "1",
-    label: <NavLink to='/admin/dashboard'>DashBoard</NavLink>
-  },
-  {
-    key: "2",
-    label:"Profile"
-  },
-  {
-    key: "3",
-    label: "user management",
-    children:[
-      {
-        key: "CreateAdmin",
-        label: <NavLink to='/admin/create-admin'>Create Admin</NavLink>,
-      },
-      {
-        key: "CreateFaculty",
-        label: <NavLink to='/admin/create-faculty'>Create Faculty</NavLink>,
-      },
-      {
-        key: "CreateStudent",
-        label: <NavLink to='/admin/create-student'>Create Student</NavLink>,
-      },
+// const items: MenuProps['items'] = [
+//   {
+//     key: "1",
+//     label: <NavLink to='/admin/dashboard'>DashBoard</NavLink>
+//   },
+//   {
+//     key: "2",
+//     label:"Profile"
+//   },
+//   {
+//     key: "3",
+//     label: "user management",
+//     children:[
+//       {
+//         key: "CreateAdmin",
+//         label: <NavLink to='/admin/create-admin'>Create Admin</NavLink>,
+//       },
+//       {
+//         key: "CreateFaculty",
+//         label: <NavLink to='/admin/create-faculty'>Create Faculty</NavLink>,
+//       },
+//       {
+//         key: "CreateStudent",
+//         label: <NavLink to='/admin/create-student'>Create Student</NavLink>,
+//       },
      
-    ]
-  }
-]
+//     ]
+//   }
+// ]
 
 const MainLayOut: React.FC = () => {
   const {
@@ -56,7 +57,7 @@ const MainLayOut: React.FC = () => {
           <h1>Ph-University</h1>
         </div>
         <div className="demo-logo-vertical" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={adminSidebarItems} />
       </Sider>
       <Layout>
         <h2></h2>
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,12 +3,18 @@ import AdminDashboard from "../pages/admin/AdminDashboard"
 import CreateAdmin from "../pages/admin/CreateAdmin"
 import CreateFaculty from "../pages/admin/CreateFaculty"
 import CreateStudent from "../pages/admin/CreateStudent"
+import { NavLink } from "react-router-dom"
 
 type TRoute = {
     path: string,
     element: ReactNode
 }
 
+type TSidebarItem ={
+    key:string;
+    label: ReactNode;
+    children?:TSidebarItem[];
+}
 export const adminPaths = [
     {
         name:"DashBoard",
@@ -48,9 +54,30 @@ export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) =>{
             acc.push({
                 path:child.path,
                 element:child.element,
-            })
-        })
+            });
+        });
     }
     return acc;
 }, [])
+
+ export const adminSidebarItems = adminPaths.reduce((acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+        acc.push({
+            key: item.name,
+            label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+        });
+    }
+    if (item.children) {
+        acc.push({
+            key: item.name,
+            label: item.name,
+            children: item.children.map((child) => ({
+                key: child.name,
+                label: <NavLink to={`${child.path}`}>{child.name}</NavLink>,
+            })),
+        });
+    }
+    return acc;
+}, []);
+
 
